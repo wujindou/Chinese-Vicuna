9df2aa63c01ad34f8128d7668f7907a5f3effa1a
JackieWu <wujindou1@163.com>
2023-04-10 17:19:45 +0800

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -6,7 +6,7 @@ TYPE_WRITER=1 # whether output streamly
 # then
 # cp ./config-sample/adapter_config.json $LORA_PATH
 # fi
-CUDA_VISIBLE_DEVICES=0 python generate.py \
+python ./generate.py \
     --model_path $BASE_MODEL \
     --lora_path $LORA_PATH \
     --use_local $USE_LOCAL \
