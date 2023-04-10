e4661c58b0ff998224b7b0d9d775325ac3f35d09
JackieWu <wujindou1@163.com>
2023-04-10 18:07:34 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -178,6 +178,7 @@ with open('/content/bloom/data/preliminary_test.csv','r',encoding='utf-8') as li
         input_str = data[1]
         idx = data[0]
         res = evaluate(instruction,input_str)
+        res = ''.join([v for v in res])
         if int(idx)>=5:continue
         print(str(idx)+','+res)
 #             if int(idx)%50==0:
